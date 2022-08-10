d9ace19d4f3f2d6cb3c5940167adbf6123ec8c9e
Gokil <76507378+googolgokil@users.noreply.github.com>
2022-08-10 19:51:17 +0530

    practice java code

---
+++ src/program10.java
@@ -0,0 +1,15 @@
+public class program10 {
+    public static void main(String[] agrs)
+    {
+        String city[] = new String[5];
+        city[0]="name";                       //progarm using foreach  loop in arrays
+        city[1]="combiatore";
+        city[2]="madurai";
+        city[3]="chennai";
+        city[4]="earth";
+        for(String x:city)
+        {
+            System.out.println(x);
+        }
+    }
+}
---
+++ src/program5.java
@@ -0,0 +1,16 @@
+// Write a Java program to swap two variables
+public class program5 {
+public static void main(String[] agrs){
+
+int a,b,c ;
+a=2;
+b=20;
+c=a;
+a=b;
+b=c;
+    System.out.println(a);
+    System.out.println(b);
+
+
+}
+}
