ffe4129127386ebfce715c60853bb9dfefa8df0b
Gokil <76507378+googolgokil@users.noreply.github.com>
2022-08-10 19:54:02 +0530

    Delete src directory

--- src/progarm20.java
+++
@@ -1,6 +0,0 @@
-public class helo{
-    public static void main(String[] agrs)
-    {
-        System.out.println("hello");
-    }
-        }
--- src/program7.java
+++
@@ -1,9 +0,0 @@
-//Write a program to print numbers from 1 to 10.
-public class program7 {
-    public static void main(String[] args) {
-        int a=2,i;
-        for( i=0;i<=10;i++){
-            System.out.println(a*i);
-        }
-    }
-}
